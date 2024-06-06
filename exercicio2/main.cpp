b7717fb7e40f6380d89a0b4df21b2fa3138e1fa7
elainelima <elainebarbosalimaa@gmail.com>
2024-06-05 22:57:29 -0300

    finalizando o ex

--- exercicio2/main.cpp
+++ exercicio2/main.cpp
@@ -10,10 +10,16 @@ using namespace std;
 
 int main(){
 
-    vector<Caminhao*> caminhao;
-    vector<Van*> van;
-    caminhao.push_back(new Caminhao("fiat","tuctuc",12,1));
-    caminhao.push_back(new Caminhao("nissan","rato",12,23));
-    van.push_back(new Van("fiat","perua",12,123));
-    van.push_back(new Van("byd","chines",33,333));
+    vector<Veiculo*> veiculo;
+    veiculo.push_back(new Caminhao("fiat","tuctuc",12,1));
+    veiculo.push_back(new Caminhao("nissan","rato",12,23));
+    veiculo.push_back(new Van("fiat","perua",12,123));
+    veiculo.push_back(new Van("byd","chines",33,333));
+    veiculo.push_back(new Van("ford","aleatorio",4,23));
+    veiculo.push_back(new Van("renaout","betoneira",776,23));
+    veiculo.push_back(new Caminhao("volkswagen","b11",76,2));
+
+    for(int i=0;i<veiculo.size();i++){
+        veiculo[i]->exibirDados();
+    }
 }
